51701d642b9b1325c687d8fd4c863f91f0da0d36
My Blockchain <trafo@op.pl>
2022-01-08 10:25:56 +0000

    org3

--- 55.privacyTest.newOrg.addNewChannel.sh
+++ 55.privacyTest.newOrg.addNewChannel.sh
@@ -12,18 +12,26 @@ setGlobals 3
 configtxgen -profile NewAppChannelEtcdRaft -configPath addNewChannel -outputBlock organizations/peerOrganizations/wrkdir/${CHANNEL}.block -channelID ${CHANNEL}
 # join channel
 setGlobals 3
-osnadmin channel list -o localhost:11080 --ca-file $ORDERER_CA --client-cert ./organizations/peerOrganizations/org3.example.com/users/Admin@org3.example.com/msp/signcerts/cert.pem --client-key ./organizations/peerOrganizations/org3.example.com/users/Admin@org3.example.com/msp/keystore/key.pem
 osnadmin channel join --channelID ${CHANNEL} --config-block organizations/peerOrganizations/wrkdir/${CHANNEL}.block -o localhost:11080 --ca-file $ORDERER_CA --client-cert ./organizations/peerOrganizations/org3.example.com/users/Admin@org3.example.com/msp/signcerts/cert.pem --client-key ./organizations/peerOrganizations/org3.example.com/users/Admin@org3.example.com/msp/keystore/key.pem
-osnadmin channel list -o localhost:11080 --ca-file $ORDERER_CA --client-cert ./organizations/peerOrganizations/org3.example.com/users/Admin@org3.example.com/msp/signcerts/cert.pem --client-key ./organizations/peerOrganizations/org3.example.com/users/Admin@org3.example.com/msp/keystore/key.pem
 peer channel join -b organizations/peerOrganizations/wrkdir/${CHANNEL}.block
-peer channel list
 #
 setGlobals 2
-osnadmin channel list -o localhost:9080 --ca-file $ORDERER_CA --client-cert ./organizations/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp/signcerts/cert.pem --client-key ./organizations/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp/keystore/key.pem
 osnadmin channel join --channelID ${CHANNEL} --config-block organizations/peerOrganizations/wrkdir/${CHANNEL}.block -o localhost:9080 --ca-file $ORDERER_CA --client-cert ./organizations/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp/signcerts/cert.pem --client-key ./organizations/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp/keystore/key.pem
-osnadmin channel list -o localhost:9080 --ca-file $ORDERER_CA --client-cert ./organizations/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp/signcerts/cert.pem --client-key ./organizations/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp/keystore/key.pem
 peer channel join -b organizations/peerOrganizations/wrkdir/${CHANNEL}.block
+#
+echo "Channels - Org1"
+setGlobals 1
+osnadmin channel list -o localhost:7080 --ca-file $ORDERER_CA --client-cert ./organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/signcerts/cert.pem --client-key ./organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/keystore/key.pem
 peer channel list
 #
+echo "Channels - Org2"
+setGlobals 2
+osnadmin channel list -o localhost:9080 --ca-file $ORDERER_CA --client-cert ./organizations/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp/signcerts/cert.pem --client-key ./organizations/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp/keystore/key.pem
+peer channel list
+#
+echo "Channels - Org3"
+setGlobals 3
+osnadmin channel list -o localhost:11080 --ca-file $ORDERER_CA --client-cert ./organizations/peerOrganizations/org3.example.com/users/Admin@org3.example.com/msp/signcerts/cert.pem --client-key ./organizations/peerOrganizations/org3.example.com/users/Admin@org3.example.com/msp/keystore/key.pem
+peer channel list
 
 cd $PWD
