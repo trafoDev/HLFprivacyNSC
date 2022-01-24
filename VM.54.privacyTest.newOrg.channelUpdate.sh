7810f856185c221212ade7a25482db2892a5ddb8
My Blockchain <trafo@op.pl>
2022-01-24 20:48:06 +0000

    newOrg

--- VM.54.privacyTest.newOrg.channelUpdate.sh
+++ VM.54.privacyTest.newOrg.channelUpdate.sh
@@ -15,7 +15,7 @@ export CHANNEL=channel.n2
 #
 setGlobals ${HOST_ORG}
 #
-peer channel update -f ${WRKDIR}/org${NEW_ORG}_update_in_envelope.pb -c ${CHANNEL} -o ${ORDERER_ADDRESS} --ordererTLSHostnameOverride ${ORDERER_HOSTNAME} --tls --cafile $ORDERER_CA
+#peer channel update -f ${WRKDIR}/org${NEW_ORG}_update_in_envelope.pb -c ${CHANNEL} -o ${ORDERER_ADDRESS} --ordererTLSHostnameOverride ${ORDERER_HOSTNAME} --tls --cafile $ORDERER_CA
 #
 peer channel fetch 0 ${WRKDIR}/channel1.block -o ${ORDERER_ADDRESS} --ordererTLSHostnameOverride ${ORDERER_HOSTNAME} -c ${CHANNEL} --tls --cafile $ORDERER_CA
 #
